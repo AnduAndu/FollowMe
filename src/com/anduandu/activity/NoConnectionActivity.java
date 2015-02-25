00070f0e5dd4268b1d06a0e5ef04534a3d845fe6
Rush <rushidadhaniya@gmail.com>
2015-02-25 22:45:22 +0530

    Internet connection status
    Internet connection status is done.

---
+++ src/com/anduandu/activity/NoConnectionActivity.java
@@ -0,0 +1,19 @@
+package com.anduandu.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.anduandu.followme.R;
+
+public class NoConnectionActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_no_connection);
+	}
+
+	
+	
+}
--- src/com/anduandu/followme/HomeScreen.java
+++ src/com/anduandu/followme/HomeScreen.java
@@ -1,42 +1,78 @@
 package com.anduandu.followme;
 
-import com.anduandu.activity.FacebookLoginActivity;
-
-import android.support.v7.app.ActionBarActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.anduandu.activity.FacebookLoginActivity;
+import com.anduandu.activity.NoConnectionActivity;
+import com.anduandu.network.NetworkUtil;
 
 public class HomeScreen extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-        
-        Intent facebookLoginActivityIntent = new Intent(this, FacebookLoginActivity.class);
-        startActivity(facebookLoginActivityIntent);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home_screen);
 
+		IntentFilter intentFilter = new IntentFilter(
+				ConnectivityManager.CONNECTIVITY_ACTION);
+		registerReceiver(networkStateReceiver, intentFilter);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_screen, menu);
-        return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	BroadcastReceiver networkStateReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			Integer type = NetworkUtil.getConnectivityStatus(context);
+			Log.d("type", type.toString());
+			if (type == 0)
+				onConnectionLost();
+			else
+				onConnectionFound();
+		}
+
+	};
+
+	private void onConnectionLost() {
+		Intent noConnectionIntent = new Intent(HomeScreen.this,
+				NoConnectionActivity.class);
+		startActivity(noConnectionIntent);
+	}
+
+	private void onConnectionFound() {
+		Intent facebookLoginIntent = new Intent(HomeScreen.this,
+				FacebookLoginActivity.class);
+		startActivity(facebookLoginIntent);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(networkStateReceiver);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.home_screen, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
